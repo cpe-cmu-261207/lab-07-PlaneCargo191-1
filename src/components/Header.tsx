2b5759acac648baf6a349f3de8fffe4a3cc555a6
PlaneCargo191 <t.james_17@hotmail.com>
2021-09-02 13:51:54 +0700

    Name updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
       <p className='text-center text-3xl italic my-2'>Pixel Painter</p>
-      <p className='text-center'>by Chayanin Suatap 610631100</p>
+      <p className='text-center'>by Thanakorn Chaininphun 620610790</p>
     </div>
   )
 }
